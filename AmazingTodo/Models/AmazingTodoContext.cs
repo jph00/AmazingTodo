a9192ad15936b0a4947c760209d29434fda3d121
Jeremy Howard <git@howard.fm>
2013-01-04 14:28:42 -0800

    MigrateDatabaseToLatestVersion

--- AmazingTodo/Models/AmazingTodoContext.cs
+++ AmazingTodo/Models/AmazingTodoContext.cs
@@ -2,14 +2,10 @@
 
 namespace AmazingTodo.Models {
     public class AmazingTodoContext : DbContext {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, add the following
-        // code to the Application_Start method in your Global.asax file.
-        // Note: this will destroy and re-create your database with every model change.
-        // 
-        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<AmazingTodo.Models.AmazingTodoContext>());
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<AmazingTodoContext, Migrations.Configuration>());
+        }
 
         public AmazingTodoContext() : base("name=AmazingTodoContext")
         {
